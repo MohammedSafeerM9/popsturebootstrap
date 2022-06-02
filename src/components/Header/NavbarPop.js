f029a1adc7775ca3e416054d9c9aac47d7d62269
Mohammed Safeer <mhdsafeerm9@gmail.com>
2022-06-02 11:01:30 +0530

    responsive

--- src/components/Header/NavbarPop.js
+++ src/components/Header/NavbarPop.js
@@ -4,21 +4,21 @@ import './NavbarPop.css';
 import logo from '../assets/images/Popsture.svg'
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
 
-var mainLogo = require('../assets/images/Popsture.svg')
+
 const NavbarPop = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" className="navbar">
-                <Container>
+                <Container className="navbar-container">
                     <img className="navbar-logo" src={ logo } alt='popsture' />
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse className="navbar-link-style">
                         <Nav className="me-auto">
-                            <Nav.Link href="#services">Services</Nav.Link>
-                            <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#projects">Careers</Nav.Link>
-                            <Nav.Link href="#projects">Contact</Nav.Link>
+                            <Nav.Link href="#services" className="ps-2">Services</Nav.Link>
+                            <Nav.Link href="#projects" className="p-2">Projects</Nav.Link>
+                            <Nav.Link href="#about" className="p-2">About</Nav.Link>
+                            <Nav.Link href="#projects" className="p-2">Careers</Nav.Link>
+                            <Nav.Link href="#projects" className="p-2" >Contact</Nav.Link>
                             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,82 +6,88 @@ import { Card } from 'react-bootstrap'
 const Homepage = () => {
     return (
         <div>
-            <div className="box1">
+            <div className="homepage-main">
                 <Container>
                     <Row>
-                        <Col></Col>
-                        <Col xs={10} className="box2 ps-2">
+                        
+                        <Col  className="box2">
                             <h1 className="hero-heading">Popsture is a Global <span>UI/UX design & branding</span> studio</h1>
                             <p className='hero-discrip'>that creates best-in-class digital experiences to help businesses succeed in <br /> today's digital world</p>
-                            <Button className="" variant="primary">Start Project</Button>{' '}
+                            <div className="button-style">
+                                <Button variant="primary">Start Project</Button>{' '}
+                            </div>
                         </Col>
-                        <Col></Col>
+
+                        
                     
                     </Row>
                     
                 </Container>       
-            </div>  
-            <div > 
-                <div className="box4">
-                    <h2 className="box7">Our Services</h2>
-                    <p>Form early stage startup to rennowed to brands,<br /> we have plenty of stories to share </p>
+            </div> 
+            <Container fluid> 
+                <div > 
+                    <div className="box4">
+                        <h2 className="box7">Our Services</h2>
+                        <p>Form early stage startup to rennowed to brands,<br /> we have plenty of stories to share </p>
+                    </div>
+                    <div className="box3" >
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px150" />
+                            <Card.Body>
+                                <Card.Text>
+                                    <h6>User studies across various demo graphics, culture to gather both qualitative & quantitative under standing of target personas</h6>
+                                </Card.Text>
+                                <Card.Text className="card-text-list">
+                                    <p>Qualitative Research</p>
+                                    <p>Quatitative Studies</p>
+                                    <p>Heuristic Evalution</p>
+                                    <p>Usability Testing</p>
+                                    <p>Competitor Analysis</p>
+                                    <p>Global  Benchmarking</p>
+                                </Card.Text>
+                                
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                
+                                <Card.Text>
+                                    <h6>Combining desgin thinking & businesses thinking to drive insights into products strategy, feature prioritisation,growth etc</h6>
+                                </Card.Text>
+                                <Card.Text className="card-text-list">
+                                    <p>Design Thinkig</p>
+                                    <p>Product Strategy</p>
+                                    <p>Product Roadmap</p>
+                                    <p>Service Design</p>
+                                    <p>Design Principals</p>
+                                    <p>Design MVP/MLP</p>
+                                </Card.Text>
+                                
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '15rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                
+                                <Card.Text>
+                                    <h6>Top notch design experiences across voice,web,mobile,AR/VR Etc to deliver value across platforms & emerging media</h6>
+                                </Card.Text>
+                                <Card.Text className="card-text-list">
+                                    <p>Branding & illustration</p>
+                                    <p>Interaction Design</p>
+                                    <p>Interface & Visual Design</p>
+                                    <p>Prototyping & Usability testing</p>
+                                    <p>Fornt end development</p>
+                                    <p>Micro interaction & motion</p>
+                                </Card.Text>
+                                
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
-                <div className="box3" >
-                    <Card style={{ width: '14.5rem' }}>
-                        <Card.Img className="bg-secondary" variant="top" src="" />
-                        <Card.Body>
-                            <Card.Text>
-                                User studies across various demo graphics, culture to gather both qualitative & quantitative under standing of target personas
-                            </Card.Text>
-                            <Card.Text>
-                                <li>Qualitative Research</li>
-                                <li>Quatitative Studies</li>
-                                <li>Heuristic Evalution</li>
-                                <li>Usability Testing</li>
-                                <li>Competitor Analysis</li>
-                                <li>Global  Benchmarking</li>
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '14.5rem' }}>
-                        <Card.Img className="bg-secondary" variant="top" src="" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Combining desgin thinking & businesses thinking to drive insights into products strategy, feature prioritisation,growth etc
-                            </Card.Text>
-                            <Card.Text>
-                                <li>Design Thinkig</li>
-                                <li>Product Strategy</li>
-                                <li>Product Roadmap</li>
-                                <li>Service Design</li>
-                                <li>Design Principals</li>
-                                <li>Design MVP/MLP</li>
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '14.5rem' }}>
-                        <Card.Img className="bg-secondary" variant="top" src="" />
-                        <Card.Body>
-                            
-                            <Card.Text>
-                                Top notch design experiences across voice,web,mobile,AR/VR Etc to deliver value across platforms & emerging media
-                            </Card.Text>
-                            <Card.Text>
-                                <li>Branding & illustration</li>
-                                <li>Interaction Design</li>
-                                <li>Interface & Visual Design</li>
-                                <li>Prototyping & Usability testing</li>
-                                <li>Fornt end development</li>
-                                <li>Micro interaction & motion</li>
-                            </Card.Text>
-                            
-                        </Card.Body>
-                    </Card>
-                </div>
-            </div>
+            </Container>
+
             <div>
                 <Container fluid className="box5 bg-danger p-5">
                     <Row>
@@ -102,6 +108,10 @@ const Homepage = () => {
                                 <p>From early stage startup to rennowed brannds,<br /> we have plenty of stories to share</p>
 
                             </div>
+                            <div>
+                            
+
+                            </div>
 
 
                         </Col>
@@ -109,65 +119,6 @@ const Homepage = () => {
                 </Container>
             </div>
             
-                    
-            {/*<div className='service_tag'>
-                <h2>Our Services</h2>
-                <p>Form early stage startup to rennowed to brands,<br /> we have plenty of stories to share </p>
-                <div className="service_container">
-                    <div className="card">
-                        <div className="card__header">
-                            <img src="" alt="Research"  width="500" height="200"></img>
-                        </div>
-                        <div className="card__body">
-                            <p>User studies across various demo graphics, culture to gather both qualitative & quantitative under standing of target personas</p>
-                            
-                                <li>Qualitative Research</li>
-                                <li>Quatitative Studies</li>
-                                <li>Heuristic Evalution</li>
-                                <li>Usability Testing</li>
-                                <li>Competitor Analysis</li>
-                                <li>Global  Benchmarking</li>
-                            
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card__header">
-                            <img src="" alt="Desgin"  width="500" height="200"></img>
-                        </div>
-                        <div className="card__body">
-                            
-                            <p>Combining desgin thinking & businesses thinking to drive insights into products strategy, feature prioritisation,growth etc</p>
-                            
-                            <li>Design Thinkig</li>
-                            <li>Product Strategy</li>
-                            <li>Product Roadmap</li>
-                            <li>Service Design</li>
-                            <li>Design Principals</li>
-                            <li>Design MVP/MLP</li>
-                            
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card__header">
-                            <img src="" alt="Build"  width="500" height="200"></img>
-                        </div>
-                        <div className="card__body">
-                            
-                            <p>Top notch design experiences across voice,web,mobile,AR/VR Etc to deliver value across platforms & emerging media</p>
-                            
-                            
-                            
-                        </div>
-                    </div>
-                
-                </div>
-                </div>*/}
-            
-            {/*<div className='video__container'>
-                <video  autoPlay muted>
-                    <source src="videos/sample-mp4-file (1).mp4" type="video/mp4" />
-                </video>
-            </div>*/}
         </div>
     )
 }
